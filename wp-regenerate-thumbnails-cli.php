7495cce95a050e04814670f7772b50c35d5d8f62
Vladimir Gorbunov <vsg@suburban.me>
2015-10-21 11:08:32 +0300

    Add license

--- wp-regenerate-thumbnails-cli.php
+++ wp-regenerate-thumbnails-cli.php
@@ -1,4 +1,10 @@
 <?php
+/*
+  Copyright © 2015 Vladimir Gorbunov <vsg@suburban.me>
+  This work is free. You can redistribute it and/or modify it under the
+  terms of the Do What The Fuck You Want To Public License, Version 2,
+  as published by Sam Hocevar. See the COPYING file for more details.
+*/
 namespace WpRegenerateThumbnailsCli;
 
 class RegeneratorException extends \Exception { }
